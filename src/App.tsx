3096241393cf78450f1a509f155bf802f75e2045
euvitor <vitoramorimdias@outlook.com>
2026-04-13 17:38:52 -0300

    feat: implement read, create and update task operations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react'
-import { fetchTasks, type Task } from './services/tasks'
+import React, { useEffect, useState } from 'react'
+import { createTask, fetchTasks, updateTask, type Task } from './services/tasks'
+
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([])
+  const [newTaskTitle, setNewTaskTitle] = useState<string>('')
 
   console.log(tasks)
 
@@ -16,15 +18,37 @@ function App() {
 
   }, [])
 
+  async function newTaskHandler(e: React.FormEvent) {
+    e.preventDefault()
+
+    const newTask = await createTask(newTaskTitle)
+    setTasks(prev => [...prev, newTask])
+    setNewTaskTitle('')
+  }
+
+  async function taskDoneHandler(id: string, done: boolean) {
+    const updatedTask = await updateTask(id, !done)
+    setTasks(prev => prev.map(t => t.id === id ? updatedTask : t))
+  }
+
   return (
     <>
-      <div className='newTaskContainer'>
-        <label htmlFor="newTaskInput"></label>
-        <input type="text" placeholder='to-do task here' />
-      </div>
+      <form onSubmit={newTaskHandler} className='newTaskContainer'>
+        <label htmlFor="newTaskInput">New Task:</label>
+        <input type="text" placeholder='to-do task here' value={newTaskTitle} onChange={(e) => setNewTaskTitle(e.target.value)} />
+        <input type="submit" value="Save" />
+      </form>
       <div className='taskList'>
         {tasks.map((val: Task) => (
-          <p>{val.title} <br /></p>
+          <div key={val.id}>
+            <input
+              type="checkbox"
+              id={val.id}
+              checked={val.done}
+              onChange={() => taskDoneHandler(val.id, val.done)}
+            />
+            <label htmlFor={val.id}>{val.title}</label>
+          </div>
         ))}
       </div >
     </>
--- src/services/tasks.ts
+++ src/services/tasks.ts
@@ -10,9 +10,38 @@ export interface Task {
 }
 
 export async function fetchTasks(): Promise<Task[]> {
-    const { data, error } = await supabase.from('tasks').select('*')
+    const { data, error } = await supabase
+        .from('tasks')
+        .select('*')
 
     if (error) throw error
 
     return data ?? []
+}
+
+export async function createTask(taskTitle: string): Promise<Task> {
+    const { data, error } = await supabase
+        .from('tasks')
+        .insert([
+            { title: taskTitle },
+        ])
+        .select()
+        .single()
+
+    if (error) throw error
+
+    return data
+}
+
+export async function updateTask(id: string, done: boolean): Promise<Task> {
+    const { data, error } = await supabase
+        .from('tasks')
+        .update({ done: done })
+        .eq('id', id)
+        .select()
+        .single()
+
+    if (error) throw error
+
+    return data
 }
