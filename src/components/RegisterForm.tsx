c5cd67076ecb60b09f045a85cfaa7518840e8048
euvitor <vitoramorimdias@outlook.com>
2026-04-16 18:19:52 -0300

    doublechecking file saves before switch pc where i'm developing

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -21,6 +21,25 @@ export function RegisterForm() {
 
     return (
         <>
+            <form action="">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" onChange={(e) => setName(e.target.value)} />
+                {errors.name ?? <p>{errors.name}</p>}
+
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} />
+                {errors.email ?? <p>{errors.email}</p>}
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                {errors.password ?? <p>{errors.password}</p>}
+
+                <label htmlFor="confirmPassword">Confirm password</label>
+                <input type="password" name="confirmPassword" onChange={(e) => setConfirmPassword(e.target.value)} />
+                {errors.confirmPassword ?? <p>{errors.confirmPassword}</p>}
+
+                <input type="submit" value="Register" />
+            </form>
         </>
     )
 }
